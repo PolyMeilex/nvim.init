59b22c2ec8793c72f76bc4473e709348a042a6f6
PolyMeilex <marynczakbartlomiej@gmail.com>
2023-12-14 19:21:21 +0100

    Got to next/prev hunk

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -34,6 +34,14 @@ return require('packer').startup(function(use)
     cond = IsNotVsCode,
     config = function()
       require('gitsigns').setup()
+
+      vim.keymap.set('n', 'tg', function()
+        require('gitsigns').next_hunk()
+      end, {})
+
+      vim.keymap.set('n', 'tG', function()
+        require('gitsigns').prev_hunk()
+      end, {})
     end,
   }
 
