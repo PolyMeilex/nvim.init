757160c243f08b19025cbb763ba7ac67d1eb50a1
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-06-21 20:15:16 +0200

    Drop comment plugin in favor of new buildin commenting

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,21 @@
 local IsNotVsCode = require('vscode').IsNotVsCode()
 
+-- TODO: Remove after next update, that includes those spaces by default
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  desc = 'Force commentstring to include spaces',
+  callback = function()
+    local cs = vim.bo.commentstring
+
+    if not cs or not cs:match('%%s') then
+      return
+    end
+
+    vim.bo.commentstring = cs:gsub('%s*%%s%s*', ' %%s '):gsub('%s*$', '')
+  end,
+})
+
+-- ft.vala = { '//%s', '/*%s*/' }
+-- ft.wgsl = { '//%s', '/*%s*/' }
+
 return {
-  'numToStr/Comment.nvim',
-  enabled = IsNotVsCode,
-  config = function()
-    require('Comment').setup()
-    local ft = require('Comment.ft')
-    ft.vala = { '//%s', '/*%s*/' }
-    ft.wgsl = { '//%s', '/*%s*/' }
-  end
 }
