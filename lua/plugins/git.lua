8fc590f57ba5781e92a0fb9560c222e40d1296f4
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-08-26 22:45:44 +0200

    Cleaner right aligned git blame label

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,12 +5,21 @@ return {
     'f-person/git-blame.nvim',
     enabled = IsNotVsCode,
     config = function()
+      require("gitblame").setup({
+        message_template = "<author>, <date>: <summary>",
+        date_format = "%r",
+        set_extmark_options = {
+          virt_text_pos = "right_align",
+        },
+      })
+
       vim.cmd "GitBlameDisable"
     end
   },
   {
     'echasnovski/mini.diff',
     version = '*',
+    enabled = IsNotVsCode,
     config = function()
       require('mini.diff').setup({
         view = {
@@ -23,7 +32,7 @@ return {
         },
         options = {
           wrap_goto = true,
-        }
+        },
       })
     end
   },
