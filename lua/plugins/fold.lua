2c0f647ff0132517d6db37dddd678aeb386809ed
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-09-05 21:58:08 +0200

    Drop ufo.nvim and just use treesitter folds

---
+++ lua/plugins/fold.lua
@@ -0,0 +1,8 @@
+vim.o.foldcolumn = '0'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ''
+
+return {}
