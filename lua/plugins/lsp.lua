097bda6f89c43bfd768e8edef72f0c6865702485
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-09-18 20:04:00 +0200

    Fixup lsp capabilities table

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 local IsNotVsCode = require('vscode').IsNotVsCode()
 
 local function lsp_capabilities()
-  return vim.tbl_extend(
+  return vim.tbl_deep_extend(
     "force",
     vim.lsp.protocol.make_client_capabilities(),
     require('cmp_nvim_lsp').default_capabilities(),
