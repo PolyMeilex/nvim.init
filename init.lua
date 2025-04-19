4e6177247115acf7d6fb5a33383f5cb8465f650a
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-04-19 19:24:44 +0200

    Remove deprecated vim.highlight

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = "YankIncSearch" })
+    vim.hl.on_yank({ higroup = "YankIncSearch" })
   end,
 })
 
