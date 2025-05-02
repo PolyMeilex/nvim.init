bb83f214f771de2efa2377e93d8e7379dc19766f
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-05-03 01:20:08 +0200

    Use `vim.lsp.config` instead of `lspconfig.server_name.setup`

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,38 +1,64 @@
-local IsNotVsCode = require("vscode").IsNotVsCode()
+vim.diagnostic.config({
+  virtual_text = {
+    severity = vim.diagnostic.severity.ERROR,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+    },
+  },
+})
 
-local function lsp_capabilities()
-  return vim.tbl_deep_extend(
-    "force",
-    vim.lsp.protocol.make_client_capabilities(),
-    require("mini.completion").get_lsp_capabilities(),
-    {
-      textDocument = {
-        foldingRange = {
-          dynamicRegistration = false,
-          lineFoldingOnly = true,
-        },
-        codeLens = {
-          dynamicRegistration = true,
-        },
-      },
-      experimental = {
-        commands = {
-          commands = {
-            "rust-analyzer.runSingle",
-            -- "rust-analyzer.debugSingle",
-            -- "rust-analyzer.gotoLocation",
-            "rust-analyzer.showReferences",
-          },
-        },
-      },
-    }
-  )
-end
+vim.api.nvim_create_user_command("LspToggleInlayHints", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+end, {})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP attach actions",
+  callback = function(event)
+    local bufnr = event.buf
+
+    ---@type vim.lsp.Client|nil
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client == nil then
+      return
+    end
+
+    local telescope = require("telescope.builtin")
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", telescope.lsp_definitions, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", telescope.lsp_implementations, opts)
+    vim.keymap.set("n", "go", telescope.lsp_type_definitions, opts)
+    vim.keymap.set("n", "gr", telescope.lsp_references, opts)
+
+    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
+
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", function()
+      vim.diagnostic.jump({ count = -1, severity = { min = vim.diagnostic.severity.WARN } })
+    end, opts)
+    vim.keymap.set("n", "]d", function()
+      vim.diagnostic.jump({ count = 1, severity = { min = vim.diagnostic.severity.WARN } })
+    end, opts)
+    vim.keymap.set("n", "[D", function()
+      vim.diagnostic.jump({ count = -1, severity = { min = vim.diagnostic.severity.ERROR } })
+    end, opts)
+    vim.keymap.set("n", "]D", function()
+      vim.diagnostic.jump({ count = 1, severity = { min = vim.diagnostic.severity.ERROR } })
+    end, opts)
+  end,
+})
 
 return {
   {
     "folke/lazydev.nvim",
-    enabled = IsNotVsCode,
     ft = "lua",
     opts = {
       library = {
@@ -61,69 +87,13 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    enabled = IsNotVsCode,
     config = function()
-      local lspconfig = require("lspconfig")
-
-      vim.diagnostic.config({
-        virtual_text = {
-          severity = vim.diagnostic.severity.ERROR,
-        },
-        -- virtual_lines = {
-        --   current_line = true,
-        -- },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = " ",
-            [vim.diagnostic.severity.WARN] = " ",
-            [vim.diagnostic.severity.INFO] = " ",
-            [vim.diagnostic.severity.HINT] = "󰌵",
-          },
-        },
-      })
-
-      vim.api.nvim_create_user_command("LspToggleInlayHints", function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
-      end, {})
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        desc = "LSP attach actions",
-        callback = function(event)
-          local bufnr = event.buf
-
-          ---@type vim.lsp.Client|nil
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client == nil then
-            return
-          end
-
-          local telescope = require("telescope.builtin")
-          local opts = { buffer = bufnr }
-
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gd", telescope.lsp_definitions, opts)
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gi", telescope.lsp_implementations, opts)
-          vim.keymap.set("n", "go", telescope.lsp_type_definitions, opts)
-          vim.keymap.set("n", "gr", telescope.lsp_references, opts)
-
-          vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-          vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
-
-          vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "[d", function()
-            vim.diagnostic.jump({ count = -1, severity = { min = vim.diagnostic.severity.WARN } })
-          end, opts)
-          vim.keymap.set("n", "]d", function()
-            vim.diagnostic.jump({ count = 1, severity = { min = vim.diagnostic.severity.WARN } })
-          end, opts)
-          vim.keymap.set("n", "[D", function()
-            vim.diagnostic.jump({ count = -1, severity = { min = vim.diagnostic.severity.ERROR } })
-          end, opts)
-          vim.keymap.set("n", "]D", function()
-            vim.diagnostic.jump({ count = 1, severity = { min = vim.diagnostic.severity.ERROR } })
-          end, opts)
-        end,
+      vim.lsp.config("*", {
+        capabilities = vim.tbl_deep_extend(
+          "force",
+          vim.lsp.protocol.make_client_capabilities(),
+          require("mini.completion").get_lsp_capabilities()
+        ),
       })
 
       -- TODO: Try to merge in https://github.com/JohnnyMorganz/StyLua/pull/970 and add this to nvim lspconfig
@@ -131,20 +101,30 @@ return {
         cmd = { "stylua", "--lsp" },
         filetypes = { "lua" },
       }
-      vim.lsp.enable("stylua-lsp-rs")
 
-      for _, server_name in pairs({ "yamlls", "pyright", "html", "lua_ls", "clangd", "ts_ls", "taplo", "dartls" }) do
-        lspconfig[server_name].setup({ capabilities = lsp_capabilities() })
-      end
-
-      lspconfig.typos_lsp.setup({
-        capabilities = lsp_capabilities(),
+      vim.lsp.config("typos_lsp", {
         init_options = {
           diagnosticSeverity = "Hint",
         },
       })
-      lspconfig.rust_analyzer.setup({
-        capabilities = lsp_capabilities(),
+
+      for _, server_name in pairs({
+        "yamlls",
+        "pyright",
+        "html",
+        "lua_ls",
+        "clangd",
+        "ts_ls",
+        "taplo",
+        "dartls",
+        "stylua-lsp-rs",
+        "typos_lsp",
+        "rust_analyzer",
+      }) do
+        vim.lsp.enable(server_name)
+      end
+
+      vim.lsp.config("rust_analyzer", {
         settings = {
           ["rust-analyzer"] = {
             checkOnSave = true,
@@ -175,18 +155,10 @@ return {
                     description = "Wrap the expression in a `Result::Ok`",
                     scope = "expr",
                   },
-                  ["Box::pin"] = {
-                    postfix = "pinbox",
-                    body = "Box::pin(${receiver})",
-                    requires = "std::boxed::Box",
-                    description = "Put the expression into a pinned `Box`",
-                    scope = "expr",
-                  },
-                  ["Arc::new"] = {
-                    postfix = "arc",
-                    body = "Arc::new(${receiver})",
-                    requires = "std::sync::Arc",
-                    description = "Put the expression into an `Arc`",
+                  ["Err"] = {
+                    postfix = "err",
+                    body = "Err(${receiver})",
+                    description = "Wrap the expression in a `Result::Err`",
                     scope = "expr",
                   },
                   ["Some"] = {
@@ -195,10 +167,11 @@ return {
                     description = "Wrap the expression in an `Option::Some`",
                     scope = "expr",
                   },
-                  ["Err"] = {
-                    postfix = "err",
-                    body = "Err(${receiver})",
-                    description = "Wrap the expression in a `Result::Err`",
+                  ["Arc::new"] = {
+                    postfix = "arc",
+                    body = "Arc::new(${receiver})",
+                    requires = "std::sync::Arc",
+                    description = "Put the expression into an `Arc`",
                     scope = "expr",
                   },
                   ["Rc::new"] = {
@@ -211,15 +184,6 @@ return {
                 },
               },
             },
-            -- lens = {
-            --   enable = true,
-            --   implementations = { enable = true },
-            --   references = {
-            --     adt = { enable = true },
-            --     trait = { enable = true },
-            --   },
-            --   run = { enable = true },
-            -- },
           },
         },
       })
