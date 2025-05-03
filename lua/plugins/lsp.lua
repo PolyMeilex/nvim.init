9dfeb88e4e07ad24bf7c68227ff052c477138443
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-05-03 04:34:50 +0200

    Open lsp completion on InsertCharPre

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,8 +64,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
       return
     end
 
+    vim.api.nvim_create_autocmd("InsertCharPre", {
+      buffer = args.buf,
+      -- TODO: Debounce?
+      callback = vim.schedule_wrap(vim.lsp.completion.get),
+    })
+
     vim.lsp.completion.enable(true, client.id, args.buf, {
-      autotrigger = true,
+      autotrigger = false,
       convert = function(item)
         local kind = vim.lsp.protocol.CompletionItemKind[item.kind] or "Unknown"
         local icon = LspItemTypeIcons[item.kind] or ""
@@ -80,7 +86,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 vim.keymap.set("i", "<c-n>", vim.lsp.completion.get)
-vim.o.completeopt = "menu,menuone,noinsert"
+vim.o.completeopt = "menuone,noinsert"
 
 return {
   {
