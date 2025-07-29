2d9c73c84906e7e1d5804f50e8e1ba18a1ed3fc6
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-07-29 23:09:58 +0200

    Add wgsl comment string

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,10 @@
-local IsNotVsCode = require("vscode").IsNotVsCode()
-
--- TODO: Remove after next update, that includes those spaces by default
 vim.api.nvim_create_autocmd({ "FileType" }, {
   desc = "Force commentstring to include spaces",
   callback = function()
     local cs = vim.bo.commentstring
 
     if not cs or not cs:match("%%s") then
-      if vim.bo.filetype == "dart" then
+      if vim.bo.filetype == "dart" or vim.bo.filetype == "wgsl" then
         vim.bo.commentstring = "// %s"
       end
 
@@ -17,8 +14,5 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.bo.commentstring = cs:gsub("%s*%%s%s*", " %%s "):gsub("%s*$", "")
   end,
 })
-
--- ft.vala = { '//%s', '/*%s*/' }
--- ft.wgsl = { '//%s', '/*%s*/' }
 
 return {}
