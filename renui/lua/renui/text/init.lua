1c37c9637142603bfa268f739b29062a35bf546c
PolyMeilex <marynczakbartlomiej@gmail.com>
2026-01-23 04:09:25 +0100

    renui: Cleanup Text class

--- renui/lua/renui/text/init.lua
+++ renui/lua/renui/text/init.lua
@@ -1,32 +1,18 @@
-local _ = require("renui.utils")._
-
----@class nui_text_extmark
----@field id? integer
----@field hl_group? string
----@field [string] any
-
 ---@class RenuiText
----@field protected extmark? nui_text_extmark
+---@field protected extmark? vim.api.keyset.set_extmark
 local Text = {}
 Text.__index = Text
 
----@param content string|RenuiText text content or NuiText object
----@param extmark? string|nui_text_extmark highlight group name or extmark options
+---@param content string text content or NuiText object
+---@param extmark? string|vim.api.keyset.set_extmark highlight group name or extmark options
 function Text:new(content, extmark)
   local this = setmetatable({}, self)
-
-  if type(content) == "string" then
-    this:set(content, extmark)
-  else
-    -- cloning
-    this:set(content._content, extmark or content.extmark)
-  end
-
+  this:set(content, extmark)
   return this
 end
 
 ---@param content string text content
----@param extmark? string|nui_text_extmark highlight group name or extmark options
+---@param extmark? string|vim.api.keyset.set_extmark highlight group name or extmark options
 ---@return RenuiText
 function Text:set(content, extmark)
   if self._content ~= content then
@@ -38,7 +24,13 @@ function Text:set(content, extmark)
   if extmark then
     -- preserve self.extmark.id
     local id = self.extmark and self.extmark.id or nil
-    self.extmark = type(extmark) == "string" and { hl_group = extmark } or vim.deepcopy(extmark)
+
+    if type(extmark) == "string" then
+      self.extmark = { hl_group = extmark }
+    else
+      self.extmark = vim.deepcopy(extmark)
+    end
+
     self.extmark.id = id
   end
 
@@ -74,8 +66,4 @@ function Text:highlight(bufnr, ns_id, linenr, byte_start)
   self.extmark.id = vim.api.nvim_buf_set_extmark(bufnr, ns_id, linenr - 1, byte_start, self.extmark)
 end
 
----@alias NuiText.constructor fun(content: string|RenuiText, extmark?: string|nui_text_extmark): RenuiText
----@type RenuiText|NuiText.constructor
-local NuiText = Text
-
-return NuiText
+return Text
